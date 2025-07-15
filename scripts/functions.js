da4d939a3202a885f0d94c3eb3cf3d912d7672f1
adamp <adam.piskorek@gmx.de>
2025-07-15 20:51:31 +0200

    last bugs fixet

--- scripts/functions.js
+++ scripts/functions.js
@@ -11,14 +11,16 @@ function likedFunction(j) {
 }
 
 function commentAdd(j) {
-    if (document.getElementById('myInputNameId').value == '') { document.getElementById('myInputNameId').focus(); document.getElementById('myInputNameId').classList.toggle('letsRed');booksVar[2]=j }
+    if (document.getElementById('myInputNameId').value == '') { document.getElementById('myInputNameId').focus(); document.getElementById('myInputNameId').classList.toggle('letsRed'); booksVar[2] = j }
     else {
-        let myUserName = document.getElementById('myInputNameId').value; let myCommentContent = document.getElementById('myInputId' + j).value
-        let myComment = `{"name": "${myUserName}", "comment": "${myCommentContent}"}`
-        myComment = JSON.parse(myComment)
-        books[j].comments.splice(0, 0, myComment);
-        saveToLocalStorage();
-        checkCheckbox(j)
+        if (document.getElementById('myInputId' + j).value == '') { document.getElementById('myInputId' + j).focus(); document.getElementById('myInputId' + j).classList.toggle('letsRed') } else {
+            let myUserName = document.getElementById('myInputNameId').value; let myCommentContent = document.getElementById('myInputId' + j).value
+            let myComment = `{"name": "${myUserName}", "comment": "${myCommentContent}"}`
+            myComment = JSON.parse(myComment)
+            books[j].comments.splice(0, 0, myComment);
+            saveToLocalStorage();
+            checkCheckbox(j)
+        }
     }
 }
 
@@ -37,8 +39,8 @@ function loadFromLocalStorage(i) {
         if (localStorage.getItem('books') != null && localStorage.getItem('booksVar') != null) {
             books = JSON.parse(localStorage.getItem('books'));
             booksVar = JSON.parse(localStorage.getItem('booksVar'));
-            document.getElementById('myInputNameId').value=booksVar[0]
-            document.getElementById('checkboxId').value=booksVar[1]
+            document.getElementById('myInputNameId').value = booksVar[0]
+            document.getElementById('checkboxId').value = booksVar[1]
             if (document.getElementById('checkboxId').checked) { renderBooksContent4OneBook(i) } else { renderBooksContent() };
         } else { saveToLocalStorage() }
     }
@@ -53,9 +55,9 @@ function myOnclickRight(l) {
     { renderBooksContent4OneBook(l) }
 }
 
-function goBack(){
-    booksVar[0]=document.getElementById('myInputNameId').value
+function goBack() {
+    booksVar[0] = document.getElementById('myInputNameId').value
     saveToLocalStorage();
-    myElement='myInputId'+booksVar[2]
+    myElement = 'myInputId' + booksVar[2]
     document.getElementById(myElement).focus()
 }
